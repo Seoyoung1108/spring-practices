01d46e23a8a74d541bf6276fcab38b58e091c78f
BIT <BIT@BIT1>
2024-12-26 14:49:50 +0900

    spring emaillist 12/26

---
+++ emaillist03/src/main/java/emaillist/controller/EmaillistController.java
@@ -0,0 +1,22 @@
+package emaillist.controller;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import emaillist.repository.EmaillistRepository;
+import emaillist.vo.EmaillistVo;
+
+@Controller
+public class EmaillistController {
+	private EmaillistRepository emaillistRepository;
+	
+	@RequestMapping("/")
+	public String index(Model model) {
+		List<EmaillistVo> list = emaillistRepository.findAll();
+		model.addAttribute("list", list);
+		return "index";  // spring-servlet.xml에 viewresolver를 적용
+	}
+}
