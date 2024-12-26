bc7d2a3456545ce31f92144dc9d359a57abaa668
BIT <BIT@BIT1>
2024-12-26 16:47:38 +0900

    spring emaillist 12/26

--- emaillist03/src/main/java/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/emaillist/controller/EmaillistController.java
@@ -2,6 +2,7 @@ package emaillist.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,12 +12,29 @@ import emaillist.vo.EmaillistVo;
 
 @Controller
 public class EmaillistController {
+	
+	// @Autowired // null이어도 에러 안터지게
 	private EmaillistRepository emaillistRepository;
+	
+	public EmaillistController(EmaillistRepository emaillistRepository) { // null이어도 에러 안터지게
+		this.emaillistRepository = emaillistRepository;
+	}
 	
 	@RequestMapping("/")
 	public String index(Model model) {
 		List<EmaillistVo> list = emaillistRepository.findAll();
 		model.addAttribute("list", list);
 		return "index";  // spring-servlet.xml에 viewresolver를 적용
+	}
+	
+	@RequestMapping("/form")
+	public String form() {
+		return "form";
+	}
+	
+	@RequestMapping("/add")
+	public String add(EmaillistVo vo) {
+		emaillistRepository.insert(vo);
+		return "redirect:/";
 	}
 }
