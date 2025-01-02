1a2b5d0e710d1ac18ae6ce6f56a982d7bfe46c14
BIT <BIT@BIT1>
2025-01-02 15:38:01 +0900

    multipart 01/02

---
+++ fileupload/src/main/java/fileupload/controller/FileuploadController.java
@@ -0,0 +1,12 @@
+package fileupload.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class FileuploadController {
+	@RequestMapping({"","/form"})
+	public String form() {
+		return "form";
+	}
+}
