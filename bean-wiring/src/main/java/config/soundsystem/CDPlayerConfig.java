27fdc4c2c392ba76a884cb2fb56b724c6d3178f3
BIT <BIT@BIT1>
2025-01-10 11:13:31 +0900

    auto configuration 01/10

---
+++ bean-wiring/src/main/java/config/soundsystem/CDPlayerConfig.java
@@ -0,0 +1,13 @@
+package config.soundsystem;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import soundsystem.CompactDisc;
+
+@Configuration
+@ComponentScan(basePackages={"soundsystem"})
+public class CDPlayerConfig {
+	
+}
---
+++ bean-wiring/src/main/java/soundsystem/CompactDisc.java
@@ -0,0 +1,6 @@
+package soundsystem;
+
+public interface CompactDisc {
+	String play();
+	
+}
